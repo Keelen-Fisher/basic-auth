4296a7423278f044788b56c78e188867e7f700f7
Keelen Fisher <keelencarrera@gmail.com>
2022-09-26 22:50:46 -0500

    setting up routes and application structure

--- src/server.js
+++ src/server.js
@@ -11,10 +11,23 @@ const { Sequelize, DataTypes } = require('sequelize');
 // connect postgres for local dev environment and prod
 // handle SSL requirements
 // connect with sqlite::memory for testing
-const DATABASE_URL = 'sqlite::memory'
 
-// Prepare the express app
 const app = express();
+// Prepare the express app
+const PORT = process.env.PORT || 3002;
+
+const DATABASE_URL = process.env.NODE_ENV === 'test'
+  ? 'sqlite::memory' // two colons allows for NO persistance
+  : 'sqlite:memory';  // one colon allows us to persist - useful today
+
+
+let options = process.env.NODE_ENV === 'production' ? {
+  dialectOptions: {
+    ssl: true,
+    rejectUnauthorized: false,
+  },
+} : {};
+
 
 // Process JSON input and put the data on req.body
 app.use(express.json());
@@ -33,27 +46,74 @@ const Users = sequelize.define('User', {
   password: {
     type: DataTypes.STRING,
     allowNull: false,
-  }
+  },
 });
 
+
+async function basicAuth(req, res, next) {
+  let { authorization } = req.headers;
+  console.log('authorization::::', authorization);
+  // confirm request header has an "authorization" property
+  if (!authorization) {
+    res.status(401).send('Not Authorized');
+  } else {
+    // Parse the basic auth string
+    let authString = authorization.split(' ')[1];
+    console.log('authStr:', authString); // dGVzdDpwYXNz
+
+    let decodedAuthString = base64.decode(authString);
+    console.log('decodedAuthString:', decodedAuthString); // test:pass
+
+    let [ username, password ] = decodedAuthString.split(':');
+    console.log('username:', username);
+    console.log('password:', password);
+
+    // find the user in the database
+    let user = await Users.findOne({where: { username }});
+    console.log('user:', user);
+    // IF the user exists (in database after a signup request)...
+    if(user){
+      // compare  password from database to the signin password 
+      // note: password could also be sent from a logged in client 
+      let validUser = await bcrypt.compare(password, user.password);
+      console.log('validUser', validUser);
+      // if valid user DOES exist... 
+      if(validUser){
+        // attach user to the request object
+        req.user = user;
+        // basicAuth middleware is done, pass request to next middleware
+        next();
+        // if valid user DOES NOt exist...
+      } else {
+        // send a "Not Authorized" error to express middleware
+        next('Not Authorized');
+      }
+    }
+
+  }
+}
 // Signup Route -- create a new user
 // Two ways to test this route with httpie
 // echo '{"username":"john","password":"foo"}' | http post :3000/signup
 // http post :3000/signup username=john password=foo
-app.post('/signup', async (req, res) => {
+app.post('/signup', async (req, res, next) => {
 
   try {
-    req.body.password = await bcrypt.hash(req.body.password, 10);
+    req.body.password = await bcrypt.hash(req.body.password, 5);
     const record = await Users.create(req.body);
     res.status(200).json(record);
-  } catch (e) { res.status(403).send('Error Creating User'); }
+  }
+  catch (error) 
+  { 
+    res.status(403).send('Error Creating User'); 
+  }
 });
 
 
 // Signin Route -- login with username and password
 // test with httpie
 // http post :3000/signin -a john:foo
-app.post('/signin', async (req, res) => {
+app.post('/signin', basicAuth, async (req, res) => {
 
   /*
     req.headers.authorization is : "Basic sdkjdsljd="
@@ -93,7 +153,7 @@ app.post('/signin', async (req, res) => {
 // make sure our tables are created, start up the HTTP server.
 sequelize.sync()
   .then(() => {
-    app.listen(3000, () => console.log('server up'));
+    app.listen(PORT, () => console.log('server up'));
   }).catch(e => {
     console.error('Could not start server', e.message);
   });
